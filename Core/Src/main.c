b55130a96379299ef48da4babe9f2a90c7169b1e
DominikZurawski <38438473+DominikZurawski@users.noreply.github.com>
2025-09-21 19:42:15 +0200

    Added MSI clock 8Mhz configuration

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,46 +1,78 @@
 #include "stm32l4xx_hal.h"
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 
 #define TRUE 1
 #define FALSE 0
 
-void SystemClockConfig(void);
 void UART2_Init(void);
 void Error_handler(void);
-uint8_t convert_to_capital(uint8_t data);
 
 UART_HandleTypeDef huart2;
 
-char *user_data = "The application is running\r\n";
-uint8_t  data_buffer[100];
-uint8_t  recvd_data;
-uint32_t count=0;
-uint8_t  reception_complete = FALSE;
-
-
 int main(void)
 {
-    HAL_Init();
-    SystemClockConfig();
+	RCC_OscInitTypeDef osc_init;
+	RCC_ClkInitTypeDef clk_init;
+	char msg[100];
+	HAL_Init();
+
+	memset(&osc_init,0,sizeof(osc_init));
+	osc_init.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+    osc_init.MSIState = RCC_MSI_ON;
+    osc_init.MSIClockRange = RCC_MSIRANGE_6; // 4 MHz
+    osc_init.MSICalibrationValue = 0;
+    osc_init.PLL.PLLState   = RCC_PLL_NONE;
+
+    if ( HAL_RCC_OscConfig(&osc_init) != HAL_OK)
+    {
+        Error_handler();
+    }
+
+
+    clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK  | \
+                        RCC_CLOCKTYPE_PCLK1  | RCC_CLOCKTYPE_PCLK2;
+    clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+    clk_init.AHBCLKDivider = RCC_SYSCLK_DIV2;
+    clk_init.APB1CLKDivider = RCC_HCLK_DIV1;
+    clk_init.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if( HAL_RCC_ClockConfig(&clk_init, FLASH_ACR_LATENCY_0WS) != HAL_OK)
+    {
+        Error_handler();
+    }
+
+    /*---------------------------- AFTER THIS LINE SYSCLK is SOURCED BY HSE------------------*/
+
+    __HAL_RCC_HSI_DISABLE(); //Saves some current
+
+    /* LETS REDO THE SYSTICK CONFIGURATION */
+
+    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
     UART2_Init();
 
-    uint16_t len_of_data = strlen(user_data);
-	HAL_UART_Transmit(&huart2,(uint8_t*)user_data,len_of_data,HAL_MAX_DELAY);
+	memset(msg,0,sizeof(msg));
+	sprintf(msg,"SYSCLK : %ldHz\r\n",HAL_RCC_GetSysClockFreq());
+	HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
 
+	memset(msg,0,sizeof(msg));
+	sprintf(msg,"HCLK   : %ldHz\r\n",HAL_RCC_GetHCLKFreq());
+	HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
 
-    while(reception_complete != TRUE)
-    {
-        HAL_UART_Receive_IT(&huart2,&recvd_data,1);
-    }
+	memset(msg,0,sizeof(msg));
+	sprintf(msg,"PCLK1  : %ldHz\r\n",HAL_RCC_GetPCLK1Freq());
+	HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
+
+	memset(msg,0,sizeof(msg));
+	sprintf(msg,"PCLK2  : %ldHz\r\n",HAL_RCC_GetPCLK2Freq());
+	HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
 
 	while(1);
 
-    return 0;
-}
-
-void SystemClockConfig(void)
-{
+	return 0;
 }
 
 void UART2_Init(void)
@@ -61,29 +93,17 @@ void UART2_Init(void)
 
 void Error_handler(void)
 {
-	while(1);
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    GPIO_InitTypeDef gpio_led;
+    gpio_led.Pin = GPIO_PIN_5;
+    gpio_led.Mode = GPIO_MODE_OUTPUT_PP;
+    gpio_led.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOA, &gpio_led);
+
+    while(1)
+    {
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+        HAL_Delay(200);
+    }
 }
 
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-{
-  if(recvd_data == '\r')
-  {
-    reception_complete = TRUE;
-    data_buffer[count++]='\r';
-    HAL_UART_Transmit(huart,data_buffer,count,HAL_MAX_DELAY);
-  }
-  else
-  {
-    data_buffer[count++] = recvd_data;
-  }
-}
-
-uint8_t convert_to_capital(uint8_t data)
-{
-	if( data >= 'a' && data <= 'z')
-	{
-		data = data - ( 'a'- 'A');
-	}
-
-	return data;
-}
